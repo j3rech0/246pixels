390abe678fa5d6f13bb31d0fc2a9c866363ff7b6
Jech <j3re.ch0@gmail.com>
2024-01-24 01:11:32 +0800

    logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
+
+const isProd = import.meta.env.PROD;
+
+const SERVE_NETLIFY = {
+  output: "server",
+  adapter: netlify(),
+};
+
+const SERVE_AMPLIFY = {};
+
+const config = isProd ? SERVE_AMPLIFY : SERVE_NETLIFY;
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig(config);
+
+console.log(config);
